40d873be06aa204d364e9e9a10f7e6faed528d84
Christian Castillo <castillochristian3@gmail.com>
2022-11-22 18:04:18 -0500

    commit tree 449e7c3b8c1b0ad18fad7b92e0b069b63eeee7fd

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[ :name ]
+      capital_name = params[ :name ].capitalize()
+      students_matching_capital_name =  Student.all.filter{ |student| student.first_name == capital_name || student.last_name == capital_name }
+      render json: students_matching_capital_name
+    else
+      students = Student.all
+      render json: students
+    end
+  end
+
+  def show
+    student = Student.find( params[ :id ] )
+    render json: student
   end
 
 end
